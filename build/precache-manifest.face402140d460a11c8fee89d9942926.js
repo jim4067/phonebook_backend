1343bb050f07d07bf0075ab4a1ff82fb0efc1ba4
jim4067 <jimmyimpulse2@gmail.com>
2020-06-16 14:44:30 +0300

    uibuild

---
+++ build/precache-manifest.face402140d460a11c8fee89d9942926.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "699036521afaf11cd90657052360d078",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9cb34bb2af546bcb922",
+    "url": "/static/css/main.e285df07.chunk.css"
+  },
+  {
+    "revision": "a3cf6be0e04478ef40d0",
+    "url": "/static/js/2.494cef1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.494cef1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9cb34bb2af546bcb922",
+    "url": "/static/js/main.364be638.chunk.js"
+  },
+  {
+    "revision": "958485858e5a4e276e09",
+    "url": "/static/js/runtime-main.872caf22.js"
+  }
+]);
